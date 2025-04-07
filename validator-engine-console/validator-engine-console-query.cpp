a389f3b09f15bf999971a67831f8de86f47a8dc7
Priyansh Rathi <prathi@chorus.one>
2025-04-07 16:18:15 +0530

    Implement querying validator-engine version from validator-engine-console

--- validator-engine-console/validator-engine-console-query.cpp
+++ validator-engine-console/validator-engine-console-query.cpp
@@ -1596,3 +1596,21 @@ td::Status DelShardQuery::receive(td::BufferSlice data) {
   td::TerminalIO::out() << "successfully removed shard\n";
   return td::Status::OK();
 }
+
+td::Status GetVersionQuery::run() {
+  TRY_STATUS(tokenizer_.check_endl());
+  return td::Status::OK();
+}
+
+td::Status GetVersionQuery::send() {
+  auto b = ton::create_serialize_tl_object<ton::ton_api::engine_validator_getVersion>();
+  td::actor::send_closure(console_, &ValidatorEngineConsole::envelope_send_query, std::move(b), create_promise());
+  return td::Status::OK();
+}
+
+td::Status GetVersionQuery::receive(td::BufferSlice data) {
+  TRY_RESULT_PREFIX(f, ton::fetch_tl_object<ton::ton_api::engine_validator_version>(data.as_slice(), true),
+                    "received incorrect answer: ");
+  td::TerminalIO::out() << f->version_ << "\n";
+  return td::Status::OK();
+}
