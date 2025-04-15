3c2255e26559a9346bd8e3dc6cc5aac0a9b81890
Priyansh Rathi <prathi@chorus.one>
2025-04-15 16:39:26 +0800

    Add GetVersion query runner to Validator Engine Console

--- validator-engine-console/validator-engine-console.cpp
+++ validator-engine-console/validator-engine-console.cpp
@@ -179,6 +179,7 @@ void ValidatorEngineConsole::run() {
   add_query_runner(std::make_unique<QueryRunnerImpl<ShowShardBlockVerifierConfigQuery>>());
   add_query_runner(std::make_unique<QueryRunnerImpl<SetConsensusNoncriticalParamsOverridesQuery>>());
   add_query_runner(std::make_unique<QueryRunnerImpl<GetConsensusNoncriticalParamsOverridesQuery>>());
+  add_query_runner(std::make_unique<QueryRunnerImpl<GetVersionQuery>>());
 }
 
 bool ValidatorEngineConsole::envelope_send_query(td::BufferSlice query, td::Promise<td::BufferSlice> promise) {
