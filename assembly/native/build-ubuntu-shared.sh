91120018deb4aa24e08d748db057e973e7130187
Léo Gillot-Lamure <lgillotlamure@chorus.one>
2025-04-30 15:38:40 +0200

    C1 private patch: force -march=x86-64-v2

--- assembly/native/build-ubuntu-shared.sh
+++ assembly/native/build-ubuntu-shared.sh
@@ -53,6 +53,7 @@ else
 fi
 
 cmake -GNinja -DTON_USE_JEMALLOC=ON .. \
+-DTON_ARCH=x86-64-v2 \
 -DCMAKE_BUILD_TYPE=Release \
 -DOPENSSL_ROOT_DIR=$opensslPath \
 -DOPENSSL_INCLUDE_DIR=$opensslPath/include \
