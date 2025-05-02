e7182df51f342bc922c18941cef2670b2473a151
Léo Gillot-Lamure <lgillotlamure@chorus.one>
2025-05-02 09:41:23 +0200

    C1 private patch: build for generic -march=x86-64-v2 instead of native

--- assembly/native/build-ubuntu-shared.sh
+++ assembly/native/build-ubuntu-shared.sh
@@ -53,6 +53,8 @@ else
 fi
 
 cmake -GNinja -DTON_USE_JEMALLOC=ON .. \
+-DPORTABLE=1 \
+-DTON_ARCH=x86-64-v2 \
 -DCMAKE_BUILD_TYPE=Release \
 -DOPENSSL_ROOT_DIR=$opensslPath \
 -DOPENSSL_INCLUDE_DIR=$opensslPath/include \
